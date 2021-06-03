773c9ea9743c6707ae8b6d0f549334ad4354bea8
KJW <57553647+nogosan@users.noreply.github.com>
2021-06-03 18:44:46 +0900

    got mess
    need to be reverted

--- netflix_clone_coding/lib/main.dart
+++ netflix_clone_coding/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_coding/screen/home_screen.dart';
+import 'package:netflix_clone_coding/screen/loading_screen.dart';
 import 'package:netflix_clone_coding/screen/more_screen.dart';
 import 'package:netflix_clone_coding/widget/bottom_bar.dart';
 
@@ -12,31 +16,61 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      _error = true;
+    }
+  }
+
   late TabController controller;
+
+  _MyAppState() {
+    initializeFlutterFire();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'JinFlix',
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.black,
-          accentColor: Colors.white,
-        ),
-        home: DefaultTabController(
-            length: 4,
-            child: Scaffold(
-              body: TabBarView(
-                  physics: NeverScrollableScrollPhysics(),
-                  children: <Widget>[
-                    HomeScreen(),
-                    Container(child: Center(child: Text('search'))),
-                    Container(child: Center(child: Text('save'))),
-                    MoreScreen()
-                  ]
-              ),
-              bottomNavigationBar: Bottom(),
-            )
-        )
-    );
+
+    if(_error) {
+      return exit(1);
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return LoadingScreen();
+    }
+
+    return myAwesomeApp();
   }
+}
+
+Widget myAwesomeApp() {
+  return MaterialApp(
+      title: 'JinFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: <Widget>[
+                  HomeScreen(),
+                  Container(child: Center(child: Text('search'))),
+                  Container(child: Center(child: Text('save'))),
+                  MoreScreen()
+                ]),
+            bottomNavigationBar: Bottom(),
+          )));
 }
--- netflix_clone_coding/lib/screen/home_screen.dart
+++ netflix_clone_coding/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_coding/model/model_movie.dart';
 import 'package:netflix_clone_coding/widget/box_slider.dart';
@@ -9,32 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies = [
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-  ];
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late List<Movie> movies;
 
   @override
   void initState() {
